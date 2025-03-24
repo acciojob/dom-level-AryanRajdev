992d237360db76b5aab34948d4d93a3e220d5b88
AryanRajdev <91838247+AryanRajdev@users.noreply.github.com>
2025-03-24 15:58:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+function getLevel(){
+	let element = document.querySelector("#level");
+
+	let level = 1;
+	while(element.parentNode){
+		level++;
+		element = element.parentNode;
+	}
+
+	alert(`The level of the element is: ${level-1}`);
+}
+
+getLevel();
